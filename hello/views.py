18f0d38c36e667fd215ff7b0e12ab4b14c67742f
gerryfrank10 <gerryfrank720@gmail.com>
2020-08-29 23:51:58 +0300

    Display Hello World

--- hello/views.py
+++ hello/views.py
@@ -8,9 +8,9 @@ import requests
 def index(request):
     # return HttpResponse('Hello from Python!')
     # return render(request, "index.html")
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>'+ r.text + '</pre>')
+    #r = requests.get('http://httpbin.org/status/418')
+    #print(r.text)
+    return HttpResponse('<p>'+ "Hello World " + '</p>')
 
 def db(request):
 
