380169e0f5f6f0869ed53d64b15cb48c884bffec
gerryfrank10 <gerryfrank720@gmail.com>
2020-09-14 15:13:12 +0300

    Created a real app

--- DocBook/urls.py
+++ DocBook/urls.py
@@ -1,4 +1,4 @@
-"""herokutest URL Configuration
+"""DocBook URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
