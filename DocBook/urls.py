71ae2161d22e27883d4f1d69209e23a1d479bd3c
gerryfrank10 <gerryfrank720@gmail.com>
2020-09-23 08:27:45 +0300

    Updated the media upload directory in settings and urls

--- DocBook/urls.py
+++ DocBook/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from hospital import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 router = routers.DefaultRouter()
 router.register(r'hospitals', views.HospitalView, 'hospital')
@@ -16,4 +19,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hospital.urls')),
     path('api/',include(router.urls))
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
